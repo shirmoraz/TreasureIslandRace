9166a0524e7e92fe3da6b3bc83cc3efc03db3492
Shir Moraz <shirmoraz2002@gmail.com>
2026-09-02 19:51:25 +0300

    rtl fix

--- TreasureIslandRace/Forms/SquareEditForm.Designer.cs
+++ TreasureIslandRace/Forms/SquareEditForm.Designer.cs
@@ -40,16 +40,19 @@
             // 
             // lblSquareIndex
             // 
-            lblSquareIndex.Location = new Point(20, 20);
+            lblSquareIndex.Location = new Point(5, 19);
             lblSquareIndex.Name = "lblSquareIndex";
+            lblSquareIndex.RightToLeft = RightToLeft.No;
             lblSquareIndex.Size = new Size(260, 20);
             lblSquareIndex.TabIndex = 0;
-            lblSquareIndex.Text = "משבצת מספר: -";
+            lblSquareIndex.Text = "משבצת מספר:";
+            lblSquareIndex.TextAlign = ContentAlignment.TopRight;
             // 
             // label1
             // 
-            label1.Location = new Point(20, 55);
+            label1.Location = new Point(205, 55);
             label1.Name = "label1";
+            label1.RightToLeft = RightToLeft.Yes;
             label1.Size = new Size(60, 20);
             label1.TabIndex = 1;
             label1.Text = "סוג:";
@@ -59,23 +62,23 @@
             // 
             cmbSquareType.DropDownStyle = ComboBoxStyle.DropDownList;
             cmbSquareType.FormattingEnabled = true;
-            cmbSquareType.Location = new Point(90, 52);
+            cmbSquareType.Location = new Point(23, 56);
             cmbSquareType.Name = "cmbSquareType";
             cmbSquareType.Size = new Size(180, 23);
             cmbSquareType.TabIndex = 2;
             // 
             // label2
             // 
-            label2.Location = new Point(20, 90);
+            label2.Location = new Point(205, 90);
             label2.Name = "label2";
-            label2.RightToLeft = RightToLeft.No;
+            label2.RightToLeft = RightToLeft.Yes;
             label2.Size = new Size(60, 20);
             label2.TabIndex = 3;
             label2.Text = "פרמטר:";
             // 
             // numParam
             // 
-            numParam.Location = new Point(90, 87);
+            numParam.Location = new Point(123, 87);
             numParam.Maximum = new decimal(new int[] { 35, 0, 0, 0 });
             numParam.Name = "numParam";
             numParam.Size = new Size(80, 23);
