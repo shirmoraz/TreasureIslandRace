7b7af38320dc4b23bd04c227b60c74641365c2aa
Shir Moraz <shirmoraz2002@gmail.com>
2026-09-05 19:57:07 +0300

    line

--- TreasureIslandRace/Forms/MainForm.cs
+++ TreasureIslandRace/Forms/MainForm.cs
@@ -384,6 +384,9 @@ namespace TreasureIslandRace.Forms
 
             int cellSize = GetCellSize();
             int index = GetSquareIndexAt(e.Location, cellSize);
+
+
+
             if (index < 0 || index >= board.Squares.Count) return;
 
             if (e.Button == MouseButtons.Right)
